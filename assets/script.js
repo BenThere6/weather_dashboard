59ef5a446c7bb53dd188e56c712263ee246da3d4
BenThere <benjaminbirdsall@icloud.com>
2023-08-07 16:21:15 -0600

    added apis

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,58 @@
-city_search = document.getElementById('city_search');
-city = document.getElementById('city');
-today_date = document.getElementById('today_date');
-today_icon = document.getElementById('today_icon');
-today_temp = document.getElementById('today_temp');
-today_wind = document.getElementById('today_wind');
-today_humidity = document.getElementById('today_humidity');
-day_one_date = document.getElementById('day_one_date');
-day_one_icon = document.getElementById('day_one_icon');
-day_one_temp = document.getElementById('day_one_temp');
-day_one_wind = document.getElementById('day_one_wind');
-day_one_humidity = document.getElementById('day_one_humidity');
-day_two_date = document.getElementById('day_two_date');
-day_two_icon = document.getElementById('day_two_icon');
-day_two_temp = document.getElementById('day_two_temp');
-day_two_wind = document.getElementById('day_two_wind');
-day_two_humidity = document.getElementById('day_two_humidity');
-day_three_date = document.getElementById('day_three_date');
-day_three_icon = document.getElementById('day_three_icon');
-day_three_temp = document.getElementById('day_three_temp');
-day_three_wind = document.getElementById('day_three_wind');
-day_three_humidity = document.getElementById('day_three_humidity');
-day_four_date = document.getElementById('day_four_date');
-day_four_icon = document.getElementById('day_four_icon');
-day_four_temp = document.getElementById('day_four_temp');
-day_four_wind = document.getElementById('day_four_wind');
-day_four_humidity = document.getElementById('day_four_humidity');
-day_five_date = document.getElementById('day_five_date');
-day_five_icon = document.getElementById('day_five_icon');
-day_five_temp = document.getElementById('day_five_temp');
-day_five_wind = document.getElementById('day_five_wind');
-day_five_humidity = document.getElementById('day_five_humidity');
+var city_search = document.getElementById('city_search');
+var city = document.getElementById('city');
+var today_date = document.getElementById('today_date');
+var today_icon = document.getElementById('today_icon');
+var today_temp = document.getElementById('today_temp');
+var today_wind = document.getElementById('today_wind');
+var today_humidity = document.getElementById('today_humidity');
+var day_one_date = document.getElementById('day_one_date');
+var day_one_icon = document.getElementById('day_one_icon');
+var day_one_temp = document.getElementById('day_one_temp');
+var day_one_wind = document.getElementById('day_one_wind');
+var day_one_humidity = document.getElementById('day_one_humidity');
+var day_two_date = document.getElementById('day_two_date');
+var day_two_icon = document.getElementById('day_two_icon');
+var day_two_temp = document.getElementById('day_two_temp');
+var day_two_wind = document.getElementById('day_two_wind');
+var day_two_humidity = document.getElementById('day_two_humidity');
+var day_three_date = document.getElementById('day_three_date');
+var day_three_icon = document.getElementById('day_three_icon');
+var day_three_temp = document.getElementById('day_three_temp');
+var day_three_wind = document.getElementById('day_three_wind');
+var day_three_humidity = document.getElementById('day_three_humidity');
+var day_four_date = document.getElementById('day_four_date');
+var day_four_icon = document.getElementById('day_four_icon');
+var day_four_temp = document.getElementById('day_four_temp');
+var day_four_wind = document.getElementById('day_four_wind');
+var day_four_humidity = document.getElementById('day_four_humidity');
+var day_five_date = document.getElementById('day_five_date');
+var day_five_icon = document.getElementById('day_five_icon');
+var day_five_temp = document.getElementById('day_five_temp');
+var day_five_wind = document.getElementById('day_five_wind');
+var day_five_humidity = document.getElementById('day_five_humidity');
+
+var cityName = 'New York'; 
+const apiKey = '4631f6bc4da95898d19c50c6d5491e03';
+const coordsApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+// Fetch coordinates first
+fetch(coordsApiUrl)
+    .then(response => response.json())
+    .then(data => {
+        // Extract latitude and longitude from the response
+        const latitude = data.coord.lat;
+        const longitude = data.coord.lon;
+
+        // Construct the weather API URL using the extracted coordinates
+        const weatherApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
+        
+        // Fetch weather data using the constructed URL
+        return fetch(weatherApiUrl);
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // Handle the retrieved weather data here
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
